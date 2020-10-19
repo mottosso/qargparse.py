5a916e0fee1bbbb69794b8deb9e71291197ebf68
David Lai <davidlatwe@gmail.com>
2020-10-19 23:04:47 +0800

    bump version

--- qargparse.py
+++ qargparse.py
@@ -4,7 +4,7 @@ import logging
 from collections import OrderedDict as odict
 from Qt import QtCore, QtWidgets, QtGui
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 _log = logging.getLogger(__name__)
 _type = type  # used as argument
 
